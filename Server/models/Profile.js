fe7a0ab31d196cd3f69b7e3d1b3665e46ca4c516
lokeshyc01 <tradingview8901@gmail.com>
2024-02-04 16:59:09 +0530

    Profile Schema Added

---
+++ Server/models/Profile.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const profileSchema = new mongoose.Schema({
+    gender:{
+        type:String,
+        required:true
+    },
+    dateOfBirth:{
+        type:Date,
+        required:true
+    },
+    about:
+    {
+        type:String,
+        trim:true,
+    },
+    contactNumber:
+    {
+        type:Number,
+        trim:true,
+    }
+});
+
+module.exports = mongoose.model("Profile",profileSchema);
