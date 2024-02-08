c35d31fc567d0910308669b3472a82d22f9ececc
lokeshyc01 <100062082+lokeshyc01@users.noreply.github.com>
2024-02-08 14:50:08 +0530

    Auth Middleware done
    This middleware extracts the token and checks the role of that user

--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -1,14 +1,94 @@
-
-
-
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const User = require("../models/User");
 //auth
+exports.auth = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies.token ||
+      req.body.token ||
+      req.header("Authorisation").replace("Bearer ", "");
 
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token not found",
+      });
+    }
+
+    // verify token
+    try {
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+      console.log(decode);
+
+      req.user = decode;
+    } catch (error) {
+      return res.status(401).json({
+        success: false,
+        message: "token is invalid",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.staus(401).json({
+      success: false,
+      message: "something went wrong while validating the token",
+    });
+  }
+};
 
 // isStudent
 
+exports.isStudent = async (req, res, next) => {
+  try {
+    if (req.user.accountType !== "Student") {
+      return res.status(401).json({
+        success: false,
+        message: "This is protected route for Student",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "user role cannot be extracted",
+    });
+  }
+};
 
-// isStudent
+// isInstructor
 
+exports.isInstructor = async (req, res) => {
+  try {
+    if (req.user.accountType !== "Instructor") {
+      return res.status(401).json({
+        success: false,
+        message: "This is protected route for Instructor",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "user role cannot be extracted",
+    });
+  }
+};
 
 // isAdmin
-
+exports.isAdmin = async (req, res) => {
+  try {
+    if (req.user.accountType !== "Admin") {
+      return res.status(401).json({
+        success: false,
+        message: "This is protected route for Admin",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "user role cannot be extracted",
+    });
+  }
+};
